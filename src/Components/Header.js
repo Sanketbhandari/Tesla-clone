bd0603db5c71b25f5a054cf03519ca1dbc04c1e1
Sanket Bhandari <sanketbhandari22@gmail.com>
2022-02-27 23:12:24 +0530

    Main changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,11 @@
 import React, {useState} from 'react'
 import styled from "styled-components"
-import MenuIcon from '@material-ui/icons/Menu';
 import CloseIcon from '@material-ui/icons/Close';
 import { selectCars } from '../features/car/carSlice';
 import { useSelector } from 'react-redux';
 
 function Header() {
-    const [burgerStatus, setBurgerStatus] = useState(false);
+    const [MenuStatus, setMenuStatus] = useState(false);
     const cars = useSelector(selectCars);
 
   return (
@@ -14,19 +13,19 @@ function Header() {
         <a>
             <img src="/images/logo.svg" alt="" />
         </a>
-        <Menu>
+        <MenuBar>
             {cars && cars.map((car, index)=>(
                 <a key={index} href="#">{car}</a>
             ))}
-        </Menu>
+        </MenuBar>
         <RightMenu>
             <a href='#'>Shop</a>
-            <a href='#'>Tesla account</a>
-            <CustomMenu onClick={()=>setBurgerStatus(true)}/>
+            <a href='#'>Account</a>
+            <a onClick={()=>setMenuStatus(true)} href='#'>Menu</a>
         </RightMenu>
-        <BurgerNav show={burgerStatus}>
+        <MenuNav show={MenuStatus}>
             <CloseWrapper>
-            <CustomClose onClick={()=>setBurgerStatus(false)}/> 
+            <CustomClose onClick={()=>setMenuStatus(false)}/> 
             </CloseWrapper>
             {cars && cars.map((car, index)=>(
                 <li key={index}><a href='#'>{car}</a></li>
@@ -37,7 +36,7 @@ function Header() {
             <li><a href='#'>Cybertruck</a></li>
             <li><a href='#'>Roadaster</a></li>
 
-        </BurgerNav>
+        </MenuNav>
     </Container>
   )
 }
@@ -50,14 +49,16 @@ const Container = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    font-size: 14px;
     padding: 0 20px;
     top: 0;
     left: 0;
     right: 0;
     z-index: 1;
+    font-weight: 600;
 `
 
-const Menu = styled.div`
+const MenuBar = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
@@ -84,11 +85,8 @@ const RightMenu = styled.div`
     }
 `
 
-const CustomMenu = styled(MenuIcon)`
-    cursor: pointer;
-`
 
-const BurgerNav = styled.div`
+const MenuNav = styled.div`
     position: fixed;
     top: 0;
     bottom: 0;
